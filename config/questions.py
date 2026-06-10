45b96b9832d35b62d685a516f0711f21dea1371f
Elias Saffarmoghadam <esaffarmoghadam@gmail.com>
2026-06-10 10:58:46 +0200

    empty

--- config/questions.py
+++ config/questions.py
@@ -21,7 +21,7 @@ version:    26.01.20.5.08
 # >>>>>>>>>>> Easy Apply Questions & Inputs <<<<<<<<<<<
 
 # Give an relative path of your default resume to be uploaded. If file in not found, will continue using your previously uploaded resume in LinkedIn.
-default_resume_path = "./CV-Telecom.pdf"      # (In Development)
+default_resume_path = ""      # (In Development)
 
 # What do you want to answer for questions that ask about years of experience you have, this is different from current_experience?
 years_of_experience = "1"          # A number in quotes Eg: "0","1","2","3","4", etc.
